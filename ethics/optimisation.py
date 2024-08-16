a9fcd6132a43a8110fe07530f5c3631b040e45c3
aezarebski <aezarebski@gmail.com>
2024-08-16 16:46:54 +1000

    lint optimisation code

--- ethics/optimisation.py
+++ ethics/optimisation.py
@@ -1,10 +1,9 @@
 import ethics.model as em
 
-def optimal_initial_condition(a: float,
-                              b: float,
-                              model_param_id: int,
-                              burden_param_id: int,
-			      db: dict) -> int:
+
+def optimal_initial_condition(
+    a: float, b: float, model_param_id: int, burden_param_id: int, db: dict
+) -> int:
     """
     Find the optimal initial condition for a given model and
     burden parameters given the database and the a and b parameters
@@ -18,15 +17,23 @@ def optimal_initial_condition(a: float,
     # 3. Compute the loss associated with each of the outcomes given the burden parameters.
     # 4. Return the identifier of the optimal initial condition.
 
-    configs = [c for c in db["configurations"] if c["model_parameter_id"] == model_param_id]
+    configs = [
+        c for c in db["configurations"] if c["model_parameter_id"] == model_param_id
+    ]
     config_ids = [c["id"] for c in configs]
     ocs = [o for o in db["outcomes"] if o["configuration_id"] in config_ids]
-    bp = [bp for bp in db["burden_parameters"] if bp["id"] == burden_param_id][0]["parameters"]
+    bp = [bp for bp in db["burden_parameters"] if bp["id"] == burden_param_id][0][
+        "parameters"
+    ]
 
     best_ic, best_loss = None, float("inf")
     for oc in ocs:
         tmp_config = [c for c in configs if c["id"] == oc["configuration_id"]][0]
-        tmp_ic = [ic for ic in db["initial_conditions"] if ic["id"] == tmp_config["initial_condition_id"]][0]
+        tmp_ic = [
+            ic
+            for ic in db["initial_conditions"]
+            if ic["id"] == tmp_config["initial_condition_id"]
+        ][0]
         tmp_loss = em.loss(oc["outcome"], tmp_ic, bp, a, b)
         if tmp_loss < best_loss:
             best_loss = tmp_loss
--- housekeeping.sh
+++ housekeeping.sh
@@ -5,6 +5,10 @@
 black ethical_sir.py
 black scratch-demo-1.py
 black scratch.py
+black create-grid-database.py
+
+black ethics/model.py
+black ethics/optimisation.py
 
 # Keep the conda environment file up to date
 
