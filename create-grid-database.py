d65ebc0d2097d08bb37ba7a0180bb28e52e6d16f
aezarebski <aezarebski@gmail.com>
2025-01-08 14:41:48 +0000

    set up some reporting the same as the plotting script

--- create-grid-database.py
+++ create-grid-database.py
@@ -14,14 +14,22 @@ import ethics.model as em
 
 if len(sys.argv) > 1:
     config_file = sys.argv[1]
-    print(config_file)
 else:
-    print("no config file specified - using default")
-    #config_file = "config/config-2024-08-07.json"
-    #config_file = "config/config-2024-12-16_CZ_test_baseline.json"
-    config_file = "config/config-2024-12-16_CZ_test_EQTO.json"
+    config_file = "config/config-2024-10-14_manuscript.json"
+    # config_file = "config/config-2024-10-28_limited_vaccine.json"
+    # config_file = "config/config-2024-12-02_limited_low_R0.json"
 assert os.path.exists(config_file)
 
+# NOTE This assumes the configuration file is named with the format
+# `config-YYYY-MM-DD-<some_name>.json`. The `config_date_name` is used
+# as the name for the output directory.
+config_date_name = os.path.basename(config_file).replace("config-", "").replace(".json", "")
+
+
+print(70 * "=")
+print("Ethics plotting example results")
+print(f"Using config file: {config_file}")
+print(70 * "=")
 with open(config_file, "r") as f:
     CONFIG = json.load(f)
 
